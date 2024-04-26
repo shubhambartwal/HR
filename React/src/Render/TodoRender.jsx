a50d4ded3e761b7f421b9d3af147b56f5138f6f3
shubhambartwal <shubhambartwal58@gmail.com>
2024-04-27 00:04:16 +0530

    useEffect

--- React/src/Render/TodoRender.jsx
+++ React/src/Render/TodoRender.jsx
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 const TodoRender = () => {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then( async function  ( res){
-        const json=await res.json();
-        setTodos(json.todos)
-  })
+  setInterval(()=>fetch("https://sum-server.100xdevs.com/todos")
+  .then( async function  ( res){
+    const json=await res.json();
+    setTodos(json.todos)
+}),[3000])  
 
 // setTodos([{"id":4,"title":"Todo 4","description":"This is todo 4","completed":false}])
 }
