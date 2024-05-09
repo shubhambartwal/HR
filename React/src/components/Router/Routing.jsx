88711c6588cb29dab4398bc8143b71b95cc62bb4
shubhambartwal <shubhambartwal58@gmail.com>
2024-05-09 22:19:37 +0530

    routing issue resolved

--- React/src/components/Router/Routing.jsx
+++ React/src/components/Router/Routing.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  Landing  from "./Pages/Landing";
+import Landing from "./Pages/Landing";
 import Dashboard from "./Pages/Dashboard";
-const App2forRouting = () => {
+
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default App2forRouting;
+export default App;
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
-import App2forRouting from './components/Router/Routing.jsx'
+import App from './components/Router/Routing.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<App />*/}
-     {<App2forRouting/> }
+     {<App/> }
   </React.StrictMode>,
 )
